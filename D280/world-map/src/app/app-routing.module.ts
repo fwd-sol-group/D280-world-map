eccfd4427140af859a1cf720eebf0ed0e500c93c
wyatt <35381759+fwd-sol-group@users.noreply.github.com>
2023-11-10 16:45:08 -0800

    fixing routes

--- D280/world-map/src/app/app-routing.module.ts
+++ D280/world-map/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WorldComponent }, // Default route to WorldComponent
+  { path: 'world', component: WorldComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
